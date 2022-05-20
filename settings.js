5a12a949ac4cb0118b132cc3193bd9bc0b0491f6
Azwanxd123455 <105867685+Azwanxd123455@users.noreply.github.com>
2022-05-20 21:10:15 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,13 +22,13 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6288210387012','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6288210387012'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6288210387012' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'RIY' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.ownername = 'azwan' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'azwan-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
 global.footer = 'RIY' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
